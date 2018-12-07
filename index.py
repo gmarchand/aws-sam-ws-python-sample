0489994d0d857907d172b7599080d1a1105db13e
EC2 Default User <ec2-user@ip-172-31-45-133.eu-west-1.compute.internal>
2018-12-07 10:55:50 +0000

    Add DynamoDB Table

--- index.py
+++ index.py
@@ -1,10 +1,30 @@
 import json
 import datetime
+import boto3
+import os
 
+dynamodb = boto3.resource('dynamodb')
 
-def handler(event, context):
+# List all items of the DynamoDB Table
+def get(event, context):
+    print(os.environ)
+    table = dynamodb.Table(os.environ['TABLE_NAME'])
+    result = table.scan()
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World Get',
+        'body': json.dumps(result['Items']),
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
+
+# Post a new Item to the DynamoDB table
+def post(event, context):
+    table = dynamodb.Table(os.environ['TABLE_NAME'])
+    data = {
+        'output': 'Hello World Post',
+        'body' : event,
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
